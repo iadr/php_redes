1d7a501a02bd94a50bb9e2e63dfc654a2dadd52b
israel diaz <id@id.serial>
2017-12-13 15:45:20 -0300

    primer commit

---
+++ test2.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Mynewpass";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$dato = $_POST["dato"];
+$check = $_POST["check"];
+if($check=="aka"){
+  $sql = "INSERT INTO table_t (dato) VALUES ('$dato');";
+  if ($conn->query($sql) === TRUE) {
+      echo "guardado correctamente";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+}
+else{
+	echo "usuario no autorizado";	
+}
+$conn->close();
+?>

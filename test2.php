60cc884601505e82a497287dc50f732826f9e9cf
israel diaz <id@id.serial>
2017-12-13 16:38:18 -0300

    reparado2

--- test2.php
+++ test2.php
@@ -1,11 +1,10 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
+
 $dbname = "test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $dbname);
+$conn = new mysqli("localhost", "root", $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
